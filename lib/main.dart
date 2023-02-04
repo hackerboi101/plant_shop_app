a902b0f0b37c004c4afb1fac7cd019dce4a10424
hackerboi101 <asif.iq.024@gmail.com>
2023-02-05 01:31:12 +0600

    firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,11 @@ import 'package:plant_shop_app/description_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       home: HomePage(),
