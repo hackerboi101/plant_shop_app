25cc2ba6dd2d8f2182536d044b21b2690e8bb15d
hackerboi101 <asif.iq.024@gmail.com>
2023-02-05 01:12:45 +0600

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:plant_shop_app/description_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(
