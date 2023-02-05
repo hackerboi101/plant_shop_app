675f4df36961488b3fbc49979d88c11e49528bff
hackerboi101 <asif.iq.024@gmail.com>
2023-02-06 02:51:35 +0600

    Login page

--- lib/description_page.dart
+++ lib/description_page.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
-import 'package:plant_shop_app/main.dart';
+import 'package:plant_shop_app/home_page.dart';
 
 class DescriptionPage extends StatelessWidget {
   final Plant plant;
